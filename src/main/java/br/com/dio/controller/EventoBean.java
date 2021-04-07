1ec09d134f1be2d2926d8305d7b85f9149e4e4aa
mayconfuzita <mayconfuzita@gmail.com>
2021-04-06 23:58:29 -0300

    Correção @SessionScoped

--- src/main/java/br/com/dio/controller/EventoBean.java
+++ src/main/java/br/com/dio/controller/EventoBean.java
@@ -4,12 +4,14 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.dio.model.Evento;
 
 @Named
+@SessionScoped
 public class EventoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
